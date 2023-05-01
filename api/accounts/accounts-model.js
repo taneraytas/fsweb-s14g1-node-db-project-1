2d8cd3606ac1f6d6c30516d131141063b42c42a6
Taner Aytaş <120183459+taneraytas@users.noreply.github.com>
2023-05-02 00:14:03 +0300

    Tamamlandı.

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,22 +1,44 @@
+const db = require("../../data/db-config");
+
 const getAll = () => {
   // KODLAR BURAYA
-}
+  return db("accounts");
+};
 
-const getById = id => {
+const getById = (id) => {
   // KODLAR BURAYA
-}
+  return db("accounts").where("id", id).first();
+};
 
-const create = account => {
+const getByName = (name) => {
   // KODLAR BURAYA
-}
+  return db("accounts").where("name", name).first();
+};
+
+const create = (account) => {
+  // KODLAR BURAYA
+  const insertedAccount = db("accounts")
+    .insert(account)
+    .then((ids) => {
+      return getById(ids[0]);
+    });
+  return insertedAccount;
+};
 
 const updateById = (id, account) => {
   // KODLAR BURAYA
-}
+  return db("accounts")
+    .where("id", id)
+    .update(account)
+    .then((affectedRows) => {
+      return getById(id);
+    });
+};
 
-const deleteById = id => {
+const deleteById = (id) => {
   // KODLAR BURAYA
-}
+  return db("accounts").where("id", id).del();
+};
 
 module.exports = {
   getAll,
@@ -24,4 +46,5 @@ module.exports = {
   create,
   updateById,
   deleteById,
-}
+  getByName,
+};
--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+const express = require("express");
 const server = require("./api/server.js");
+
+const router = require("./api/accounts/accounts-router.js");
 
 const PORT = process.env.PORT || 9000;
 
 server.listen(PORT, () => {
   console.log(`\n== API running on port ${PORT} ==\n`);
 });
+
+server.use(express.json());
+
+server.use("/api/accounts", router);
